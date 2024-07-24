71846c5b2192fa4e26dbc1e6588313629a35cc7d
asalam <tokku.co@gmail.com>
2024-07-24 15:49:06 +0700

    add css from Tailwind/Shadcn-ui & set route w/ Router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Welcome To Tangkudev</h1>
+      <Button>Subscirbe</Button>
     </>
   )
 }
---
+++ src/create-trip/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateTrip() {
+  return (
+    <div>
+      Create Trip
+    </div>
+  )
+}
+
+export default CreateTrip
